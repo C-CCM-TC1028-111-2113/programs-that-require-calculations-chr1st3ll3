8c7cb264a4b910da80080d4250297ab64fb987e3
chr1st3ll3 <christelle_navar@hotmail.com>
2021-08-14 16:21:30 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,25 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    #This is my first program
-    #This is how I print a messag
-
-    #This is how I make a simple calculation
-    print('Give me a number')
-    number1 = int(input())
-    print('Give me a  number')
-    number2 = int(input())
-    suma=number1+number2
-    resta=number1-number2
-    multi=number1*number2
-
-    #print('The addition of these numbers is ')
-    print(suma)
-
-   # print('The substraction of these numbers is ')
-    print(resta)
-
-    #print('The multiplication of these numbers is ')
-    print(multi)
+    num1=int(input("Dame un número: "))
+    num2=int(input("Dame un número: "))
+    s = num1+num2
+    r = num1-num2
+    m = num1*num2
+    print("Suma:",s)
+    print("Resta:",r)
+    print("Multiplicación:",m)
 
 
 
