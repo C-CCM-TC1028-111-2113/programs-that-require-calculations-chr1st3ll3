e034e0b57a4de6dbdbd27ee6b794cce96d853851
chr1st3ll3 <christelle_navar@hotmail.com>
2021-08-14 15:09:55 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,34 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    #This is my first program
+    #This is how I print a message
+
+    print('Welcome to Computational Thinking class, what''s your name??')
+
+    #This is how I read a value from the user
+    yourName = input()
+
+    print('Hello ' + str(yourName))
+
+    #This is how I make a simple calculation
+    print('Give me a number')
+    number1 = int(input())
+    print('Give me a second number')
+    number2 = int(input())
+    suma=number1+number2
+    resta=number1-number2
+    multi=number1*number2
+
+    print('The addition of these numbers is ')
+    print(suma)
+
+    print('The substraction of these numbers is ')
+    print(resta)
+
+    print('The multiplication of these numbers is ')
+    print(multi)
+
+
 
 
 if __name__ == '__main__':
